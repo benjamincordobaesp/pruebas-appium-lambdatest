cc4dc6eefb171ce34e03a5d8d567790731491604
econutriflow <benjamin@nutriecoflow.fit>
2026-02-23 23:00:53 -0500

    add files

--- build/test-results/timeline/report.js
+++ build/test-results/timeline/report.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"81e9e702-9538-4627-b7aa-b3f86001f5cb","feature":"Operaciones matemáticas con la Calculadora","scenario":"Suma básica de dos números en la app nativa","start":1771903593379,"group":1,"content":"","tags":"","end":1771903595182,"className":"failed"}]);
+CucumberHTML.timelineItems.pushArray([{"id":"17715b1d-8b7d-4c63-8d20-13671f5e7221","feature":"Operaciones matemáticas con la Calculadora","scenario":"Suma básica de dos números en la app nativa","start":1771905520116,"group":1,"content":"","tags":"","end":1771905584834,"className":"failed"}]);
 CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
 });
